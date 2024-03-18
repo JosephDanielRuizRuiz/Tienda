0255b4de50abeafd155e9d2755a63a203b69a736
unknown <RGH-LS24-19@RGH-LS24-19.UMD.LOCAL.EDU>
2024-03-18 07:56:43 -0500

    Modificando archivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaComponent } from './pagina/pagina.component';
+import { TablaComponent } from './tabla/tabla.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/pagina',
+    pathMatch:'full'
+  },
+  {
+    path:'pagina',
+    component: PaginaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TablaComponent } from './tabla/tabla.component';
+import { PaginaComponent } from './pagina/pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaComponent,
+    PaginaComponent
   ],
   imports: [
     BrowserModule,
